a7e7824364a018d8b7a90f8aebbbe07d096980ea
empty-nick <85650519+empty-nick@users.noreply.github.com>
2024-01-13 02:47:46 -0800

    CAT-2 Mixin and fixes (#3)
    * feat: CAT-1 Add routing to application
    
    * feat: CAT-1 Replaced options from the Routes component
    
    * feat: CAT-2 Add mixin styles
    
    * feat: CAT-2 Add mixin styles 2
    
    * feat: CAT-2 Added index files and Mixin

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,12 @@
 import { View, Text, Image } from "react-native";
 
-export const Card = (cardData={name:"Evroopt", number:"**** 3671"}) => {
+interface ICardProps {
+  name?: string;
+  code?: string;
+}
+
+export const Card = (props: ICardProps) => {
+  const {name = "Evroopt", code = "**** 3671"} = props
     return (
         <View style={{backgroundColor: "red", padding: 5, borderRadius: 20, margin:5, display: "flex", justifyContent: "center", alignItems: "center"}}>
             <Image style={{width: 320, height: 190, borderRadius: 20}} source={require("../../../assets/123.png")}></Image>
